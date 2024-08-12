c455c21e49858d78dff80680bbfd263fd1e94140
malonej501 <jamesmalone@tutanota.com>
2024-08-12 16:51:28 +0100

    added R script to shuffle tree tips and alter character state frequencies for investigating phylogenetic hypotheses

--- phylogeny/shuffler.R
+++ phylogeny/shuffler.R
@@ -1,13 +1,14 @@
 library(ape)
 
-jan_phylo_nat_class_tree <- read.nexus("./phylogenies/final_data/trees/jan_phylo_nat_class_21-01-24.tre")
-jan_phylo_nat_class_labs <- read.table("./phylogenies/final_data/labels/jan_phylo_nat_class_21-01-24.txt", 
+jan_phylo_nat_class_tree <- read.nexus("./phylogenies/final_data/trees/geeta_phylo_geeta_class_23-04-24.tre")
+jan_phylo_nat_class_labs <- read.table("./phylogenies/final_data/labels/geeta_phylo_geeta_class_23-04-24.txt", 
                                        sep = "\t", header=FALSE, col.names = 
                                          c("taxa","shape"))
 
 # return phylogeny of equal number of each shape category, randomly selected
-min_freq_shape <- names(which.min(table(jan_phylo_nat_class_labs$shape)))
-min_freq <- min(table(jan_phylo_nat_class_labs$shape))
+shape_freqs <- jan_phylo_nat_class_labs[jan_phylo_nat_class_labs$shape %in% c(0,1,2,3), ]
+min_freq_shape <- names(which.min(table(shape_freqs$shape)))
+min_freq <- min(table(shape_freqs$shape))
 
 # generate sample
 sample <- data.frame()
@@ -21,3 +22,18 @@ for (s in c(0,1,2,3)){
 jan_phylo_nat_class_tree_sub <- drop.tip(jan_phylo_nat_class_tree, setdiff(jan_phylo_nat_class_labs$taxa, sample$taxa))
 write.nexus(jan_phylo_nat_class_tree_sub, file = "jan_phylo_nat_class_21-01-24_95_each.tre")
 write.table(sample, file = "jan_phylo_nat_class_21-91-24_95_each.txt", sep="\t", col.names = FALSE, row.names = FALSE, quote=FALSE)
+
+#shuffle phylogeny
+jan_phylo_nat_class_tree_shuff <- jan_phylo_nat_class_tree
+jan_phylo_nat_class_tree_shuff$tip.label <- sample(jan_phylo_nat_class_tree_shuff$tip.label, replace=FALSE)
+write.nexus(jan_phylo_nat_class_tree_shuff, file = "jan_phylo_nat_class_21-01-24_shuff.tre")
+
+#shuffle multiphylo
+shuffle_tree_tips <- function(tree) {
+  tree$tip.label <- sample(tree$tip.label)
+  return(tree)
+}
+
+# Apply the function to each tree in the multiphylo object
+jan_phylo_nat_class_tree_shuff <- lapply(jan_phylo_nat_class_tree_shuff, shuffle_tree_tips)
+write.nexus(jan_phylo_nat_class_tree_shuff, file = "jan_phylo_nat_class_21-01-24_shuff.tre")
