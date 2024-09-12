bbd4ebc2168740acf3a83b913e7ee1f9a6cadad5
malonej501 <jamesmalone@tutanota.com>
2024-09-12 15:03:33 +0100

    Added code to find intersect between genera and different Naturalis herbarium samples including at the species and genus level

--- phylogeny/phylogenies/Zuntini_2024/tree_analysis_code.R
+++ phylogeny/phylogenies/Zuntini_2024/tree_analysis_code.R
@@ -118,3 +118,115 @@ processed_tips <- sapply(tips, function(x) {
 zuntini_sub$tip.label <- processed_tips
 print(length(zuntini_sub$tip.label))
 write.nexus(zuntini_sub, file = "zuntini_naturalis_sub_genus_species.tre")
+
+#### Subsetting to naturalis data overall ####
+
+naturalis <- read.csv("../../leaf_databases/Naturalis/sample_eud_zuntini_10-09-24/Naturalis_multimedia_eud_sample_10-09-24.csv")
+zuntini_naturalis_intersect <- zuntini_tips[zuntini_tips$genus_species %in% intersect(zuntini_tips$genus_species, naturalis$species), ]
+#zuntini_naturalis_intersect <- zuntini_tips[zuntini_tips$genus %in% intersect(zuntini_tips$genus, naturalis$genus), ]
+length(unique(zuntini_naturalis_intersect$genus))
+naturalis_zuntini_intersect <- naturalis[naturalis$species %in% intersect(naturalis$species, zuntini_tips$genus_species),]
+naturalis_zuntini_intersect <- naturalis[naturalis$genus %in% intersect(naturalis$genus, zuntini_tips$genus),]
+length(unique(naturalis_zuntini_intersect$species))
+naturalis_zuntini_intersect <- naturalis_zuntini_intersect[!duplicated(naturalis_zuntini_intersect$genus), ]
+freqtab <- data.frame(table(naturalis_zuntini_intersect$species))
+write.csv(naturalis_zuntini_intersect, "Naturalis_multimedia_eud_sample_10-09-24_zuntini_intercept_genera.csv")
+
+
+#### Zuntini intercept with Naturalis_multimedia_eud_sample_10-09-24_zuntini_intercept_genera_labels_unambig_full data ####
+
+naturalis_labs <-
+  read.csv(
+    "Naturalis_multimedia_eud_sample_10-09-24_zuntini_intercept_labels_unambig_full.csv",
+    header = FALSE,
+    sep = "\t",
+    col.names = c("genus_species", "shape")
+  )
+naturalis_labs$genus <- gsub("_.*", "", naturalis_labs$genus_species)
+# Keep remove all but the first occurance of each genus
+#naturalis_labs <- naturalis_labs[!duplicated(naturalis_labs$genus), ]
+#naturalis_labs <- naturalis_labs[, c("genus", "shape")]
+
+# intersect between zuntini and naturalis
+zuntini_naturalis_intersect <-
+  zuntini_tips[zuntini_tips$genus_species %in% intersect(zuntini_tips$genus_species, naturalis_labs$genus_species), ]
+
+# get the resulting subset of each dataset
+naturalis_labs_sub <-
+  subset(naturalis_labs, genus_species %in% zuntini_naturalis_intersect$genus_species)
+naturalis_labs_sub <- merge(naturalis_labs_sub, zuntini_naturalis_intersect, by="genus_species")
+naturalis_labs_sub <- naturalis_labs_sub[c("order_family_genus_species","shape")]
+
+zuntini_naturalis_diff <-
+  setdiff(zuntini_tips$order_family_genus_species, naturalis_labs_sub$order_family_genus_species)
+zuntini_sub <- drop.tip(zuntini, zuntini_naturalis_diff)
+length(zuntini_sub$tip.label)
+
+#write the subset labels
+write.table(
+  naturalis_labs_sub,
+  file = "Naturalis_multimedia_eud_sample_10-09-24_zuntini_intercept_labels_unambig_full_zuntini2024_sub.txt",
+  sep = "\t",
+  row.names = FALSE,
+  col.names = FALSE,
+  quote = FALSE
+)
+# write the subset tree
+write.nexus(zuntini_sub, file = "zuntini_Naturalis_multimedia_eud_sample_10-09-24_zuntini_intercept_labels_unambig_full_sub.tre")
+
+
+#### Zuntini intercept with Naturalis_multimedia_eud_sample_10-09-24_zuntini_intercept_genera_labels_unambig_full data ####
+
+naturalis_labs <-
+  read.csv(
+    "Naturalis_multimedia_eud_sample_10-09-24_zuntini_intercept_genera_labels_unambig_full.csv",
+    header = FALSE,
+    sep = "\t",
+    col.names = c("genus_species", "shape")
+  )
+naturalis_labs$genus <- gsub("_.*", "", naturalis_labs$genus_species)
+# Keep remove all but the first occurance of each genus
+#naturalis_labs <- naturalis_labs[!duplicated(naturalis_labs$genus), ]
+#naturalis_labs <- naturalis_labs[, c("genus", "shape")]
+
+# set zuntini_phylo tips to genus
+extract_third_part <- function(string) {
+  parts <- strsplit(string, "_")[[1]]
+  if (length(parts) >= 3) {
+    return(parts[3])
+  } else {
+    return(NA)  # Return NA if there are less than 3 parts
+  }
+}
+
+zuntini$tip.label <- sapply(zuntini$tip.label, extract_third_part)
+
+zuntini_tips <- data.frame(genus = zuntini$tip.label)
+
+# intersect between zuntini and naturalis
+zuntini_naturalis_intersect <-
+  zuntini_tips[zuntini_tips$genus %in% intersect(zuntini_tips$genus, naturalis_labs$genus), ]
+
+# get the resulting subset of each dataset
+naturalis_labs_sub <-
+  subset(naturalis_labs, genus %in% zuntini_naturalis_intersect)
+naturalis_labs_sub <- merge(naturalis_labs_sub, zuntini_naturalis_intersect, by="genus")
+naturalis_labs_sub <- naturalis_labs_sub[c("genus","shape")]
+
+zuntini_naturalis_diff <-
+  setdiff(zuntini_tips$genus, naturalis_labs_sub$genus)
+zuntini_sub <- drop.tip(zuntini, zuntini_naturalis_diff)
+length(zuntini_sub$tip.label)
+
+#write the subset labels
+write.table(
+  naturalis_labs_sub,
+  file = "Naturalis_multimedia_eud_sample_10-09-24_zuntini_intercept_genera_labels_unambig_full_zuntini2024_sub.txt",
+  sep = "\t",
+  row.names = FALSE,
+  col.names = FALSE,
+  quote = FALSE
+)
+# write the subset tree
+write.nexus(zuntini_sub, file = "zuntini_Naturalis_multimedia_eud_sample_10-09-24_zuntini_intercept_labels_genera_unambig_full_sub.tre")
+
